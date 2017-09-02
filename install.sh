9f90c7fed41bbe52aaf6c81ef1e423b5998ce357
Davie Badger <davie.badger@gmail.com>
2017-09-02 21:28:38 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,151 @@
 #!/bin/bash
 #
 # * Installation
-# * Imports
+#
+#   * Packages
+#
+#     * apt
+#     * gem
+#     * pip
+#
+#   * Scripts
+#
+#     * MEGA
+#     * PowerlineFonts
+#     * VimPlug
+#
+# * Environment
+#
+#   * Configuration
+#
+#     * Bash
+#     * Git
+#     * Vim
 
 # Installation
 # ============
 
-apt install \
-    git \
-    python3-pip \
-    vim-gnome \
+# Packages
+# --------
 
-# Vim checkers
+# apt
+# ^^^
 
-apt install tidy
+apt_packages=(
+  cmake
+  git
+  python3-pip
+  tidy
+  vim-gnome
+)
 
-gem install --user-install mdl
+apt install -y ${apt_packages[@]}
 
-pip3 install --user flake8 pylint mypy docutils
+# gem
+# ^^^
 
-# Imports
-# =======
+gem_packages=(
+  mdl
+)
 
-mkdir gitlab && cd gitlab
+gem install ${gem_packages[@]}
 
-git clone https://gitlab.com/daviebadger/configs
-git clone https://gitlab.com/daviebadger/notes.git
-git clone https://gitlab.com/daviebadger/poanda.git
+# pip
+# ^^^
+
+pip_packages=(
+  docutils
+  flake8
+  mypy
+  pylint
+  virtualenv
+  virtualenvwrapper
+)
+
+pip3 install ${pip_packages[@]}
+
+# Scripts
+# -------
+
+# MEGA
+# ^^^^
+
+wget https://mega.nz/linux/MEGAsync/xUbuntu_17.04/amd64/megasync-xUbuntu_17.04_amd64.deb
+wget https://mega.nz/linux/MEGAsync/xUbuntu_17.04/amd64/nautilus-megasync-xUbuntu_17.04_amd64.deb
+
+apt install -y libc-ares2 libcrypto++6
+
+dpkg -i megasync-xUbuntu_17.04_amd64.deb
+dpkg -i nautilus-megasync-xUbuntu_17.04_amd64.deb
+
+rm megasync-xUbuntu_17.04_amd64.deb
+rm nautilus-megasync-xUbuntu_17.04_amd64.deb
+
+# PowerlineFonts
+# ^^^^^^^^^^^^^^
+
+git clone https://github.com/powerline/fonts.git --depth=1
+
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# VimPlug
+# ^^^^^^^
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Environment
+# ===========
+
+# Configuration
+# -------------
+
+mkdir gitlab
+cd gitlab
+
+git clone https://gitlab.com/daviebadger/configs.git
+
+cd configs
+
+# Bash
+# ^^^^
+
+cd bash
+
+cp bash_aliases ~/.bash_aliases
+cp shell_prompt.sh ~/.shell_prompt.sh
+
+ln -f ~/.bash_aliases bash_aliases
+ln -f ~/.shell_prompt.sh shell_prompt.sh
+
+cat < bashrc >> ~/.bashrc
+
+source ~/.bashrc
+
+cd ..
+
+# Git
+# ^^^
+
+cd git
+
+cp gitconfig ~/.gitconfig
+cp gitignore ~/.gitignore
+
+ln -f ~/.gitignore gitignore
+
+cd ..
+
+# Vim
+# ^^^
+
+cd vim
+
+cp vimrc ~/.vimrc
+ln -f ~/.vimrc vimrc
+
+mkdir ~/.vimrc/swaps/
+
+cd ..
