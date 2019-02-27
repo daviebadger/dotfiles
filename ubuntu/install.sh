820c60f2c0f5975685e036899a56fb48027f3d98
Davie Badger <davie.badger@gmail.com>
2019-02-27 22:57:10 +0100

    Load Mutter settings in Ubuntu install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,4 +5,5 @@ set -eux
 cp wallpaper.jpg ~/Pictures
 
 dconf load /com/solus-project/ < panel.dconf
+dconf load /org/gnome/mutter/ < windows.dconf
 dconf load /org/gnome/settings-daemon/ < plugins.dconf
