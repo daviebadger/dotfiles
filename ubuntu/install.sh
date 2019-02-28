75bb850b72037e29529e88d1ab3b0305e1cd98f9
Davie Badger <davie.badger@gmail.com>
2019-02-28 22:35:53 +0100

    Update key paths in Ubuntu plugins settings

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,4 +6,4 @@ cp wallpaper.jpg ~/Pictures
 
 dconf load /com/solus-project/ < panel.dconf
 dconf load /org/gnome/mutter/ < windows.dconf
-dconf load /org/gnome/settings-daemon/ < plugins.dconf
+dconf load /org/gnome/settings-daemon/plugins/ < plugins.dconf
