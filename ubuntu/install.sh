4a9b6a278d05085c22dc014ae76f302b80ff163e
Davie Badger <davie.badger@gmail.com>
2019-02-28 22:48:07 +0100

    Load Ubuntu desktop settings in install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,5 +5,6 @@ set -eux
 cp wallpaper.jpg ~/Pictures
 
 dconf load /com/solus-project/budgie-panel/ < panel.dconf
+dconf load /org/gnome/desktop/ <<< $(sed "s/davie/$USER/" desktop.dconf)
 dconf load /org/gnome/mutter/ < mutter.dconf
 dconf load /org/gnome/settings-daemon/plugins/ < plugins.dconf
