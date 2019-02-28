e015f81cb94db6a0315717f2f815daf57beb90de
Davie Badger <davie.badger@gmail.com>
2019-02-28 22:39:23 +0100

    Rename key paths in Ubuntu panel settings

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -4,6 +4,6 @@ set -eux
 
 cp wallpaper.jpg ~/Pictures
 
-dconf load /com/solus-project/ < panel.dconf
+dconf load /com/solus-project/budgie-panel/ < panel.dconf
 dconf load /org/gnome/mutter/ < mutter.dconf
 dconf load /org/gnome/settings-daemon/plugins/ < plugins.dconf
