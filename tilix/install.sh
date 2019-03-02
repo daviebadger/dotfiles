ce1732bfc092de3401443649bdd3a24dc1fba3f0
Davie Badger <davie.badger@gmail.com>
2019-03-02 11:32:21 +0100

    Refactor Tilix install script

--- tilix/install.sh
+++ tilix/install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+readonly TILIX="$(dirname "$0")"
+readonly TILIX_SESSIONS="${TILIX}/sessions"
+
 set -eux
 
-cp -fl sessions/* ~/Templates
+cp -fl "${TILIX_SESSIONS}/"* ~/Templates
 curl -sL --create-dirs -o ~/.config/tilix/schemes/davie8.json https://git.io/fhFvj
 
-dconf load /com/gexperts/Tilix/ < tilix.dconf
+dconf load /com/gexperts/Tilix/ < "${TILIX}/tilix.dconf"
