53a22cdbe43626c11fb6632954b8d466576dc43e
Davie Badger <davie.badger@gmail.com>
2019-03-02 15:49:12 +0100

    Remove Plank autostart app in Ubuntu install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,6 +6,7 @@ readonly UBUNTU_SETTINGS="${UBUNTU}/settings"
 set -eux
 
 cp "${UBUNTU}/wallpaper.jpg" ~/Pictures
+rm -f ~/.config/autostart/plank.desktop
 
 dconf load /com/solus-project/budgie-panel/ < "${UBUNTU_SETTINGS}/panel.dconf"
 dconf load /org/gnome/desktop/ <<< "$(sed "s/davie/${USER}/" "${UBUNTU_SETTINGS}/desktop.dconf")"
