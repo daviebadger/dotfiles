ae8ccc93f757f24998d094fb6257561e385187a7
Davie Badger <davie.badger@gmail.com>
2019-03-02 11:01:31 +0100

    Refactor Ubuntu install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+readonly UBUNTU="$(dirname "$0")"
+
 set -eux
 
-cp wallpaper.jpg ~/Pictures
+cp "${UBUNTU}/wallpaper.jpg" ~/Pictures
 
-dconf load /com/solus-project/budgie-panel/ < panel.dconf
-dconf load /org/gnome/desktop/ <<< $(sed "s/davie/$USER/" desktop.dconf)
-dconf load /org/gnome/mutter/ < mutter.dconf
-dconf load /org/gnome/settings-daemon/plugins/ < plugins.dconf
+dconf load /com/solus-project/budgie-panel/ < "${UBUNTU}/panel.dconf"
+dconf load /org/gnome/desktop/ <<< "$(sed "s/davie/${USER}/" "${UBUNTU}/desktop.dconf")"
+dconf load /org/gnome/mutter/ < "${UBUNTU}/mutter.dconf"
+dconf load /org/gnome/settings-daemon/plugins/ < "${UBUNTU}/plugins.dconf"
