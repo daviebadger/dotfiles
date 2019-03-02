b5ac9863768c0e8c649734048a17fc1adcc5b4f0
Davie Badger <davie.badger@gmail.com>
2019-03-02 11:14:05 +0100

    Fix paths in Ubuntu install script

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 readonly UBUNTU="$(dirname "$0")"
+readonly UBUNTU_SETTINGS="${UBUNTU}/settings"
 
 set -eux
 
 cp "${UBUNTU}/wallpaper.jpg" ~/Pictures
 
-dconf load /com/solus-project/budgie-panel/ < "${UBUNTU}/panel.dconf"
-dconf load /org/gnome/desktop/ <<< "$(sed "s/davie/${USER}/" "${UBUNTU}/desktop.dconf")"
-dconf load /org/gnome/mutter/ < "${UBUNTU}/mutter.dconf"
-dconf load /org/gnome/settings-daemon/plugins/ < "${UBUNTU}/plugins.dconf"
+dconf load /com/solus-project/budgie-panel/ < "${UBUNTU_SETTINGS}/panel.dconf"
+dconf load /org/gnome/desktop/ <<< "$(sed "s/davie/${USER}/" "${UBUNTU_SETTINGS}/desktop.dconf")"
+dconf load /org/gnome/mutter/ < "${UBUNTU_SETTINGS}/mutter.dconf"
+dconf load /org/gnome/settings-daemon/plugins/ < "${UBUNTU_SETTINGS}/plugins.dconf"
