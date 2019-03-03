19e8cd1b1a63aef06bc1d882be1cd8b1407fdec6
Davie Badger <davie.badger@gmail.com>
2019-03-03 22:23:38 +0100

    Add Neofetch install script

---
+++ neofetch/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+readonly NEOFETCH="$(dirname "$0")"
+
+set -eux
+
+sudo apt install neofetch
+
+mkdir -p ~/.config/neofetch
+cp -fl "${NEOFETCH}/config.conf" ~/.config/neofetch/
