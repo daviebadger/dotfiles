c79203c3452bf5f4bf6b4a6334f6ae177d06cae5
Davie Badger <davie.badger@gmail.com>
2019-03-03 12:21:10 +0100

    Update session destination in Tilix install script

--- tilix/install.sh
+++ tilix/install.sh
@@ -5,7 +5,9 @@ readonly TILIX_SESSIONS="${TILIX}/sessions"
 
 set -eux
 
-cp -fl "${TILIX_SESSIONS}/"* ~/Templates
 curl -sL --create-dirs -o ~/.config/tilix/schemes/davie8.json https://git.io/fhFvj
+
+mkdir -p ~/tilix
+cp -fl "${TILIX_SESSIONS}/"* ~/tilix
 
 dconf load /com/gexperts/Tilix/ < "${TILIX}/tilix.dconf"
