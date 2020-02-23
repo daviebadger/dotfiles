55dc9cbcb41a6dc556e1fc3b94af671a962ff245
Davie Badger <davie.badger@gmail.com>
2020-02-23 21:41:25 +0100

    fish: Install fisher package manager

--- fish/install.sh
+++ fish/install.sh
@@ -3,3 +3,5 @@
 sudo apt-add-repository --yes ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install --yes fish
+
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
