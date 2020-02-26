30c105e1fa65e976dbb7fc9ccbdd909419cb3c83
Davie Badger <davie.badger@gmail.com>
2020-02-26 20:37:19 +0100

    alacritty: Link alacritty.yml

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Install Alacritty, a GPU-accelerated terminal emulator.
+
+readonly LOCAL_DIR="$(dirname "${BASH_SOURCE}")"
 
 sudo add-apt-repository --yes ppa:mmstick76/alacritty
 sudo apt update
 sudo apt install install --yes alacritty
 
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
+
+ln -fv "${LOCAL_DIR}/alacritty.yml" ~/.config/alacritty/alacritty.yml
