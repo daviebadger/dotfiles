fdfcfbff62bd80bb2624bac55927fbadf348ca49
Davie Badger <davie.badger@gmail.com>
2020-02-29 22:02:19 +0100

    starship: Link starship.toml config

--- starship/install.sh
+++ starship/install.sh
@@ -2,4 +2,8 @@
 #
 # Install Starship, a cross-shell prompt.
 
+readonly LOCAL_DIR="$(dirname "${BASH_SOURCE}")"
+
 curl -fsSL https://starship.rs/install.sh | bash
+
+ln -f "${LOCAL_DIR}/starship.toml" ~/.config/starship.toml
