506a4b11916b35572271f35b2be369b617bee0f7
Davie Badger <davie.badger@gmail.com>
2020-03-01 22:31:07 +0100

    fish: Link Fish config

--- fish/install.sh
+++ fish/install.sh
@@ -2,8 +2,12 @@
 #
 # Install Fish, a user-friendly command line shell.
 
+readonly LOCAL_DIR="$(dirname $BASH_SOURCE)"
+
 sudo apt-add-repository --yes ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install --yes fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+
+ln -f "${LOCAL_DIR}/config.fish" ~/.config/fish/config.fish
