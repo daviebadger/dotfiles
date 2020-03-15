cdf6467f3f1dc5d26f5c0e5074ed08c967fea042
Davie Badger <davie.badger@gmail.com>
2020-03-15 18:11:19 +0100

    alacritty: Fix installing alacritty

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -6,7 +6,7 @@ readonly LOCAL_DIR="$(dirname $BASH_SOURCE)"
 
 sudo add-apt-repository --yes ppa:mmstick76/alacritty
 sudo apt update
-sudo apt install install --yes alacritty
+sudo apt install --yes alacritty
 
 sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
 
