495c169d94b81dab65d9f064ace155c778a350fc
Davie Badger <davie.badger@gmail.com>
2020-03-16 22:31:46 +0100

    Fix quotes in install.sh script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 # Install UbuntuMono Nerd Font with Powerline symbols and icons.
 
-echo "Installing fonts ..."
+echo 'Installing fonts ...'
 
 readonly FONT_ZIP='UbuntuMono.zip'
 readonly FONT_DIR="${HOME}/.local/share/fonts/"
