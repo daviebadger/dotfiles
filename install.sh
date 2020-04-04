191680132dfe2cd6b9b43c8ca5010cbea59a5735
Davie Badger <davie.badger@gmail.com>
2020-04-04 14:59:35 +0200

    Refactor main install.sh script

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 #
-# Install all dotfiles, including system dependencies.
+# Install all dotfiles, including system packages.
+
+set -eu
+
+readonly INSTALL="Installing %s ...\n"
 
 # Install UbuntuMono Nerd Font with Powerline symbols and icons.
 
-echo "Installing fonts ..."
+printf "${INSTALL}" fonts
 
 readonly FONT_ZIP="UbuntuMono.zip"
-readonly FONT_DIR="${HOME}/.local/share/fonts/"
+readonly FONT_DESTINATION_DIR="${HOME}/.local/share/fonts"
 
-mkdir -p $FONT_DIR
-
-curl -fsOSL "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/${FONT_ZIP}"
-unzip -q $FONT_ZIP -d $FONT_DIR
-
+curl -fsLOS "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/${FONT_ZIP}"
+mkdir -p $FONT_DESTINATION_DIR
+unzip -q $FONT_ZIP -d $FONT_DESTINATION_DIR
 rm $FONT_ZIP
 
 # Install specific applications.
 
 for dir in ./*/; do
-  echo "Installing $(basename $dir) ..."
+  printf "${INSTALL}" $(basename $dir)
 
   source "${dir}/install.sh"
 done
