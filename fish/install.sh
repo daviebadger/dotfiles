81110e2ec24235d055caf4d498c2e8770871a28e
Davie Badger <davie.badger@gmail.com>
2020-04-05 22:07:12 +0200

    fish: Ling functions

--- fish/install.sh
+++ fish/install.sh
@@ -4,10 +4,12 @@
 
 readonly SOURCE_DIR="$(dirname $BASH_SOURCE)"
 readonly DESTINATION_DIR="${HOME}/.config/fish"
+readonly DESTINATION_DIR_FUNCTIONS="${DESTINATION_DIR}/functions"
 
 sudo pacman -S --noconfirm fish
 
-mkdir -p $DESTINATION_DIR
+mkdir -p $DESTINATION_DIR $DESTINATION_DIR_FUNCTIONS
 ln -f "${SOURCE_DIR}/config.fish" $DESTINATION_DIR
+ln -f "${SOURCE_DIR}/functions/"* $DESTINATION_DIR_FUNCTIONS
 
 chsh -s /usr/bin/fish
