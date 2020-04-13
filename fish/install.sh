9df01661de64ef85ad5f5cf25b18fe5d50a0c2e8
Davie Badger <davie.badger@gmail.com>
2020-04-13 17:27:16 +0200

    fish: Fix shellcheck warnings in install.sh

--- fish/install.sh
+++ fish/install.sh
@@ -2,15 +2,15 @@
 #
 # Install Fish, a user-friendly command line shell.
 
-readonly SOURCE_DIRECTORY="$(dirname $BASH_SOURCE)"
+readonly SOURCE_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 readonly DESTINATION_DIRECTORY="${HOME}/.config/fish"
 readonly DESTINATION_DIRECTORY_FUNCTIONS="${DESTINATION_DIRECTORY}/functions"
 
 sudo pacman --sync --noconfirm fish
 
-mkdir --parents $DESTINATION_DIRECTORY
-mkdir --parents $DESTINATION_DIRECTORY_FUNCTIONS
-ln --force "${SOURCE_DIRECTORY}/config.fish" $DESTINATION_DIRECTORY
-ln --force "${SOURCE_DIRECTORY}/functions/"* $DESTINATION_DIRECTORY_FUNCTIONS
+mkdir --parents "${DESTINATION_DIRECTORY}"
+mkdir --parents "${DESTINATION_DIRECTORY_FUNCTIONS}"
+ln --force "${SOURCE_DIRECTORY}/config.fish" "${DESTINATION_DIRECTORY}"
+ln --force "${SOURCE_DIRECTORY}/functions/"* "${DESTINATION_DIRECTORY_FUNCTIONS}"
 
 chsh --shell /usr/bin/fish
