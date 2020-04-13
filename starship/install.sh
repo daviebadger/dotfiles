a1e0d5c23de0a29ac6177f1c41c0da88984352bc
Davie Badger <davie.badger@gmail.com>
2020-04-13 17:37:37 +0200

    starship: Fix shellcheck warnings in install.sh

--- starship/install.sh
+++ starship/install.sh
@@ -2,10 +2,10 @@
 #
 # Install Starship, a cross-shell prompt.
 
-readonly SOURCE_DIRECTORY="$(dirname $BASH_SOURCE)"
+readonly SOURCE_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
 readonly DESTINATION_DIRECTORY="${HOME}/.config"
 
 sudo bash -c "$(curl --fail --location --silent https://starship.rs/install.sh)" -- --yes
 
-mkdir --parents $DESTINATION_DIRECTORY
-ln --force "${SOURCE_DIRECTORY}/starship.toml" $DESTINATION_DIRECTORY
+mkdir --parents "${DESTINATION_DIRECTORY}"
+ln --force "${SOURCE_DIRECTORY}/starship.toml" "${DESTINATION_DIRECTORY}"
