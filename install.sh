f5f9a68bc1685934fa2326c60e5cdbace27e2bbd
Davie Badger <davie.badger@gmail.com>
2020-04-21 22:59:31 +0200

    font: Move installation to global install.sh for now

--- install.sh
+++ install.sh
@@ -4,13 +4,24 @@
 
 set -eu
 
-for directory in ./*/; do
-  script="${directory}/install.sh"
+# Install UbuntuMono Nerd Font with Powerline symbols and icons.
 
-  if [[ -f "${script}" ]]; then
-    echo "Installing $(basename "${directory}") ..."
+readonly FONT_ZIP="UbuntuMono.zip"
+readonly FONT_DESTINATION_DIRECTORY="${HOME}/.local/share/fonts"
 
-    # shellcheck source=/dev/null
-    source "${script}"
-  fi
-done
+curl --location --remote-name "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/${FONT_ZIP}"
+
+mkdir --parents "${FONT_DESTINATION_DIRECTORY}"
+unzip "${FONT_ZIP}" -d "${FONT_DESTINATION_DIRECTORY}"
+rm "${FONT_ZIP}"
+
+# for directory in ./*/; do
+#   script="${directory}/install.sh"
+#
+#   if [[ -f "${script}" ]]; then
+#     echo "Installing $(basename "${directory}") ..."
+#
+#     # shellcheck source=/dev/null
+#     source "${script}"
+#   fi
+# done
